647f34bdbd2dd312250c375601ecbb99cb66f1db
sky-min <81374952+sky-min@users.noreply.github.com>
2021-12-07 19:02:06 +0900

    Update BossBarAPI.php

--- src/skymin/bossbar/BossBarAPI.php
+++ src/skymin/bossbar/BossBarAPI.php
@@ -1,4 +1,26 @@
 <?php
+/**
+*      _                    _       
+*  ___| | ___   _ _ __ ___ (_)_ __  
+* / __| |/ / | | | '_ ` _ \| | '_ \ 
+* \__ \   <| |_| | | | | | | | | | |
+* |___/_|\_\\__, |_| |_| |_|_|_| |_|
+*           |___/ 
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+* 
+* @author skymin
+* @link   https://github.com/sky-min
+* @license https://opensource.org/licenses/MIT MIT License
+* 
+*   /\___/\
+* 　(∩`・ω・)
+* ＿/_ミつ/￣￣￣/
+* 　　＼/＿＿＿/
+*
+*/
+
 declare(strict_types = 1);
 
 namespace skymin\bossbar;
